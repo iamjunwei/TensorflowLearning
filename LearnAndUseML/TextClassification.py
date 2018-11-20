4a794f7f039f573b952c6bf2d819d00a8e3d61d5
joeyxia <joeyxia@tencent.com>
2018-11-20 16:30:08 +0800

    TextClassification

---
+++ LearnAndUseML/TextClassification.py
@@ -0,0 +1,68 @@
+# https://www.tensorflow.org/tutorials/keras/basic_text_classification
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+imdb = keras.datasets.imdb
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data()
+
+# each number represents a word, each data has different number of words
+# print(len(train_data[0]), len(train_data[1]))
+# print(train_data[0])
+
+word_index = imdb.get_word_index()
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index["<PAD>"] = 0
+word_index["<START>"] = 1
+word_index["<UNK>"] = 2  # unknown
+word_index["<UNUSED>"] = 3
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+
+def decode_review(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+
+print(decode_review(train_data[0]))
+
+# make input data the same size
+# Ont-hot-encoding or padding
+# choose padding
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding="post",
+                                                        maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                       value=word_index["<PAD>"],
+                                                       padding='post',
+                                                       maxlen=256)
+
+vocab_size = 100000
+model = keras.Sequential([
+    keras.layers.Embedding(vocab_size, 16),
+    keras.layers.GlobalAveragePooling1D(),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+model.summary()
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+# after about 20 epoch, validation acc is not improve, we can stop training to avoid over-fitting
+results = model.evaluate(test_data, test_labels)
+print(results)
